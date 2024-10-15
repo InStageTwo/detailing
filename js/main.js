93f0b99f37b311846e6d2f12376ed52cd7d0d6a0
InStageTwo <borzovadarian@yandex.ru>
2024-10-15 23:34:08 +0300

    fix: slider update

--- js/main.js
+++ js/main.js
@@ -1,31 +1,17 @@
 
-// const containers = document.querySelectorAll('.service-container');
-
-// containers.forEach((container) => {
-//   const slider = container.querySelector('.slider');
-
-//   if (slider) {
-//     slider.addEventListener('input', (e) => {
-//       container.style.setProperty('--position', `${e.target.value}%`);
-//     });
-//   }
-// });
-
-
-// const slider = document.getElementById("slider");
-// slider.addEventListener("input", function(e) {
-//   document.querySelector(".image.after").style.width = e.target.value + "%";
-// });
-
-const containers = document.querySelectorAll('.service-container');
-
-containers.forEach((container) => {
+document.addEventListener("DOMContentLoaded", function() {
+  const containers = document.querySelectorAll('.service-container');
+ 
+  containers.forEach((container) => {
   const slider = container.querySelector('.slider');
   const image = container.querySelector('.image.after');
-
-  if (slider && image) {
-    slider.addEventListener('input', (e) => {
-      image.style.width = `${e.target.value}%`;
-    });
-  }
+  const sliderLine = container.querySelector('.slider-line');
+ 
+    if (slider && image && sliderLine) {
+      slider.addEventListener('input', (e) => {
+        image.style.width = `${e.target.value}%`;
+        sliderLine.style.left = `${e.target.value}%`;
+      });
+    }
+  });
 });
