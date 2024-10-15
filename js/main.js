f8a9ee3a6c83d1e3dc3e4d0f9562e59dbd2c58ac
InStageTwo <borzovadarian@yandex.ru>
2024-10-15 12:46:57 +0300

    feat: add service section slider

--- js/main.js
+++ js/main.js
@@ -1,12 +1,31 @@
-const containers = document.querySelectorAll('.service-card');
+
+// const containers = document.querySelectorAll('.service-container');
+
+// containers.forEach((container) => {
+//   const slider = container.querySelector('.slider');
+
+//   if (slider) {
+//     slider.addEventListener('input', (e) => {
+//       container.style.setProperty('--position', `${e.target.value}%`);
+//     });
+//   }
+// });
+
+
+// const slider = document.getElementById("slider");
+// slider.addEventListener("input", function(e) {
+//   document.querySelector(".image.after").style.width = e.target.value + "%";
+// });
+
+const containers = document.querySelectorAll('.service-container');
 
 containers.forEach((container) => {
   const slider = container.querySelector('.slider');
-  slider.addEventListener('input', (e) => {
-    container.style.setProperty('--position', `${e.target.value}%`);
-  });
+  const image = container.querySelector('.image.after');
+
+  if (slider && image) {
+    slider.addEventListener('input', (e) => {
+      image.style.width = `${e.target.value}%`;
+    });
+  }
 });
-
-
-
-
